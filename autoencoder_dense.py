9d4e152f6e20defca4840fd448de7893fd6e1527
Marfiik <53659720+Marfiik@users.noreply.github.com>
2022-12-06 22:32:32 +0100

    Add files via upload

--- autoencoder_dense.py
+++ autoencoder_dense.py
@@ -3,36 +3,49 @@
 
 # In[1]:
 
-
+import datetime
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.layers import Dense, Normalization, Input
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping, TensorBoard
 
-def autoencoder_dense(X_train,X_test,lr=0.001,pat=3,bat_size=64,epo=50):
-    array = Input(shape=(256,))
-    norm = Normalization()(array)
-    x = Dense(128, activation="relu")(norm)
-    x = Dense(64, activation="relu")(x)
-    x = Dense(8, activation="relu")(x)
-    x = Dense(64, activation="relu")(x)
-    x = Dense(128, activation="relu")(x)
-    decoded = Dense(256, activation="relu")(x)
+def autoencoder_dense(X,Y,X_val,Y_val,lr=0.001,pat=5,bat_size=128,epo=100,inshape=256,bn=16):
+	array = Input(shape=(256,))
+	norm = Normalization()(array)
+	x = Dense(128, activation="relu")(norm)
+	x = Dense(64, activation="relu")(x)
+	x = Dense(bn, activation="relu")(x)
+	x = Dense(64, activation="relu")(x)
+	x = Dense(128, activation="relu")(x)
+	decoded = Dense(inshape, activation="relu")(x)
 
-    autoencoder = Model(array, decoded)
-    autoencoder.compile(optimizer=Adam(learning_rate=lr),
-                 loss='mean_squared_error')
+	autoencoder = Model(array, decoded)
+	autoencoder.compile(optimizer=Adam(learning_rate=lr),
+		loss='mean_squared_error')
 
-    callback_conv = [EarlyStopping(monitor='val_loss', patience=pat),
-                   TensorBoard(log_dir = "./logs/auto/{}-{}-{}-{}".format(datetime.datetime.today().month,
-                                                                          datetime.datetime.today().day,
-                                                                          datetime.datetime.today().hour,
-                                                                          datetime.datetime.today().minute), update_freq="epoch")]
-    history = autoencoder.fit(X_train, X_train,
-          validation_data = (X_test, X_test),
-                            batch_size=bat_size, 
-                            epochs=epo, 
-                            callbacks=[callback_conv])
-    return autoencoder, history
+	callback_conv = [EarlyStopping(monitor='val_loss', patience=pat),
+		TensorBoard(log_dir = "./logs/auto/{}-{}-{}-						{}".format(datetime.datetime.today().month,
+		datetime.datetime.today().day,
+		datetime.datetime.today().hour,
+		datetime.datetime.today().minute), update_freq="epoch")]
+	history = autoencoder.fit(X, Y,
+		validation_data = (X_val, Y_val),
+		batch_size=bat_size, 
+		epochs=epo, 
+		callbacks=[callback_conv],verbose=0)
+	loss = history.history['loss']
+	val_loss = history.history['val_loss']
+
+
+	plt.plot(loss, label='Training Loss')
+	plt.plot(val_loss, label='Validation Loss')
+	plt.legend(loc='upper right')
+	plt.ylabel('Cross Entropy')
+	plt.title('Training and Validation Loss')
+	plt.xlabel('epoch')
+	plt.yscale("log")
+	plt.show()
+	return autoencoder
 
--- data_gen.py
+++ data_gen.py
@@ -11,36 +11,73 @@ from sklearn.preprocessing import LabelEncoder
 def gen_signal(N, x0, w, A, bkg, peak=False):
     x = np.arange(N)
     y = np.ones(N) * bkg
-    
     if peak:
         y_peak = np.exp(-(x-x0)/w) * A
         y_peak[:x0] = 0
         y += y_peak
-        
     y = np.random.poisson(y)
+    if peak:
+        return x, y, y_peak
+    else:
+        y_peak = np.zeros(N)
+        return x, y, y_peak
 
-    return x, y
-
-def data_gen(N_data=100000,N_tu=256,x0=(64, 196),w=(5,20),A=(5,25),bkg=(5,15)):
-    data = []
-    df = pd.DataFrame(columns=["x0", "w", "A", "bkg", "GRB"], index=range(N_data))
+def data_gen(N_data=100000,N_tu=256,x0=(64, 196),w=(5,20),A=(5,20),bkg=(5,15),a=(-0.03,0.03),norm=True):
+    data, data_clean,data_slope,data_no_bkg = [],[],[],[]
+    df = pd.DataFrame(columns=["x0", "w", "A", "bkg", "GRB", "a"], index=range(N_data))
+    
+    ## GENERATING RANDOM NUMBERS FOR CONSTANTS IN GIVEN RANGE
     for i in range(N_data):
         df["x0"].loc[i] = np.random.randint(x0[0],x0[1])
         df["w"].loc[i] = np.random.uniform(w[0],w[1])
         df["A"].loc[i] = np.random.uniform(A[0],A[1])
         df["bkg"].loc[i] = np.random.uniform(bkg[0],bkg[1])
         df["GRB"].loc[i] = [True,False][np.random.randint(2)]
-        data.append(gen_signal(N=N_tu, 
-                               x0=df.x0.loc[i], 
-                               w=df.w.loc[i], 
-                               A=df.A.loc[i], 
-                               bkg=df.bkg.loc[i],
-                               peak=df.GRB.loc[i])[1])
-    X = np.array(data).reshape(-1, len(data[0]))
-    y = df.GRB
-    le = LabelEncoder() #encode target labels with value between 0 and n_classes-1
-    le.fit(y) #fit label encoder
-    y = le.transform(y) #transform labels to normalized encoding (True = 1, False = 0)
+        df["a"].loc[i] = np.random.uniform(a[0],a[1])
+        
+        
+    ## GENERATING LIGHT CURVE
+        x = gen_signal(N=N_tu, x0=df.x0.loc[i], w=df.w.loc[i], A=df.A.loc[i], bkg=df.bkg.loc[i], peak=df.GRB.loc[i])
+    
+    
+    ## GENERATING LINEARLY CHANGING BACKGROUND
+        x_slope = df.a.loc[i] * np.arange(N_tu)
+        
+        
+    ## COMBINING LC WITH AND WITHOUT BKG
+        combine = x[1]+x_slope
+        combine_no_bkg = x[2]+x_slope
+        
+        
+    ## NORMALIZING
+        if norm:
+            combine_norm = (combine-min(combine))/np.ptp(combine)
+            combine_norm_no_bkg = (combine_no_bkg-min(combine_no_bkg))/np.ptp(combine_no_bkg)
+            data.append(combine_norm)
+            data_no_bkg.append(combine_norm_no_bkg)
+            data_slope.append((x_slope-min(x_slope))/np.ptp(x_slope))
+            if np.ptp(x[2]) == 0:
+                data_clean.append(np.zeros_like(x[2]))
+            else:
+                data_clean.append((x[2]-min(x[2]))/np.ptp(x[2]))
 
-    return X, y, df
+        else:
+            data.append(combine)
+            data_clean.append(x[2])
+            data_slope.append(x_slope)
+            data_no_bkg.append(combine_norm_no_bkg)
+    
+    ## RESHAPING
+    X = np.array(data).reshape(-1, len(data[0]))
+    X_clean = np.array(data_clean).reshape(-1, len(data_clean[0]))
+    X_slope = np.array(data_slope).reshape(-1, len(data_slope[0]))
+    X_no_bkg = np.array(data_no_bkg).reshape(-1, len(data_no_bkg[0]))
+    
+    ## ENCODING (True = 1, False = 0)
+    y = df.GRB
+    le = LabelEncoder()
+    le.fit(y) 
+    y = le.transform(y) 
+
+    return X, y, df, X_clean, X_slope, X_no_bkg
 
